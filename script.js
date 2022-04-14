a39f3dfc9df07663a0f62ed210ffaf28b69377a1
Hyde Francois Khamsing <66337110+Muzika-Senpai@users.noreply.github.com>
2022-04-14 11:41:23 +0200

    Add files via upload

---
+++ script.js
@@ -0,0 +1,67 @@
+
+
+let projectButton = document.getElementById('project-button'); 
+let contactButton = document.getElementById('contact-button')
+
+let main = document.getElementById('main');
+let header = document.getElementById('header');
+let contact = document.getElementById('footer');
+
+let mainDisplay = main.style.display;
+
+function toggleMain(){
+
+    console.log ('function launched');
+
+    if( footer.style.display=="block" ){
+
+        footer.style.display="none";
+        header.style.opacity='1';
+        header.style.zIndex='1';
+    
+    }
+    
+    if( main.style.display=="block" ){
+
+        main.style.display="none";
+        header.style.opacity='1';
+    
+    }else{
+        
+        main.style.display="block";
+        header.style.opacity='0.1';
+        header.style.zIndex='-1';
+        
+    }
+
+}
+
+function toggleContact(){
+
+    console.log ('function launched');
+
+    if( main.style.display=="block" ){
+    
+        main.style.display="none";
+        header.style.opacity='1';
+        header.style.zIndex='1';
+    
+    }
+    
+    if( footer.style.display=="block" ){
+
+        footer.style.display="none";
+        header.style.opacity='1';
+    
+    }else{
+        
+        footer.style.display="block";
+        header.style.opacity='0.1';
+        header.style.zIndex='-1';
+        
+    }
+
+}
+
+contactButton.addEventListener('click',toggleContact);
+projectButton.addEventListener('click',toggleMain);
